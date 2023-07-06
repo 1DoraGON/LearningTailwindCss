c745d17b7cef9a63015af73640361605df12bf00
1DoraGON <moulaykadii2001@gmail.com>
2023-07-06 13:13:57 +0200

    hamburger menu

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,11 @@
+const initApp = () => {
+    const hamBtn = document.getElementById('hamburger-button')
+    const mobileMenu = document.getElementById('mobile-menu')
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden')
+        mobileMenu.classList.toggle('flex')
+    }
+    hamBtn.addEventListener('click', toggleMenu)
+    mobileMenu.addEventListener('click',toggleMenu)
+}
+document.addEventListener('DOMContentLoaded',initApp)
